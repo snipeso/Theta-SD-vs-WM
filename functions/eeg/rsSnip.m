a400c67efb8a1a0ecd726ac95219065af68afc70
snipeso <sophia.snipes@kispi.uzh.ch>
2021-03-31 19:07:57 +0200

    renamed functions

--- functions/eeg/rsSnip.m
+++ functions/eeg/rsSnip.m
@@ -1,15 +1,14 @@
-function rmSnippet(EEG, StartTime, EndTime, Channel)
-% function for C_Cuts in preprocessing to remove a snippet
+function rsSnip(EEG, StartTime, EndTime, Channel)
+% function for C_Cuts in preprocessing to restore a snippet
 
 m = matfile(EEG.CutFilepath,'Writable',true);
 
 Content = whos(m);
 
-if ~ismember('cutData', {Content.name})
-    m.cutData = nan(size(EEG.data)); % for plotting purposes
-end
-
 fs = EEG.srate;
+[ch, pnts] = EEG.data;
+
+% get latencies in points
 Start = round(StartTime*fs);
 End = round(EndTime*fs);
 
@@ -26,4 +25,7 @@ if Channel < 1 || Channel > ch
     error('Not a real channel')
 end
 
-m.cutData(Channel, Start:End) = EEG.data(Channel, Start:End);
+% set back to nan the values of the snippet matrix
+if ismember('cutData', {Content.name})
+    m.cutData(Channel, Start:End) = nan;
+end
