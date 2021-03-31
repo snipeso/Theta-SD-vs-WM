e66d3c1b26c28bc01cb5fbded8436bf3fea76504
snipeso <sophia.snipes@kispi.uzh.ch>
2021-03-31 21:47:55 +0200

    scripts for removing manually ICA components

---
+++ Preprocessing/E_RemoveComponents.m
@@ -0,0 +1,105 @@
+% This script is for manually removing components containing noise. Click
+% run. A popup will appear with the top 35 components of a randomly
+% selected file. Select the bad components, then press ok. 2 popups will
+% show the new data; evaluate if it is sufficiently cleaned. When prompted
+% in the editor, indicate 'y' for accepting selection, 'n' for rejecting it
+% and running it again, and 's' to skip and do a different one.
+
+% if you indicate "automate", it will just apply the selection and removal
+% of components already done to the indicated datatype.
+
+clear
+eeglab % needs to be run every time to clear global variables
+close all
+clc
+Prep_Parameters
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Data_Type = 'Power';
+% Filename = ['P01_Game_Session2_ICA_Components.set'];
+% Task = 'Match2Sample';
+Filename = [];
+
+CheckOutput = true; % manually verify if selection was good at the end
+Automate = false; % automatically apply previous selection of components to Data_Type (used when applying to ERP data)
+Refresh = false; % redo already done files
+
+Component_Folder = 'Components'; % 'Components';
+Destination_Folder = 'Deblinked'; % 'Deblinked'
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% choose a random task
+if ~exist('Task', 'var') || isempty(Task)
+    Task = allTasks{randi(numel(allTasks))};
+end
+
+% get files and paths
+
+load('Cz.mat', 'CZ')
+
+Source_Comps = fullfile(Paths.Preprocessed, 'ICA', Component_Folder, Task);
+Source_Data = fullfile(Paths.Preprocessed, Data_Type, 'SET', Task);
+Destination = fullfile(Paths.Preprocessed, 'ICA', [Destination_Folder, '_',Data_Type], Task);
+
+if ~exist(Destination, 'dir')
+    mkdir(Destination)
+end
+
+Files = deblank(cellstr(ls(Source_Comps)));
+Files(~contains(Files, '.set')) = [];
+
+% randomize files list
+nFiles = numel(Files);
+Files = Files(randperm(nFiles));
+
+for Indx_F = 1:nFiles % loop through files in source folder
+    
+    %%% get filenames
+    
+    if isempty(Filename)
+        Filename_Comps = Files{Indx_F};
+    else
+        Filename_Comps = Filename;
+    end
+    
+    Filename_Data = replace(Filename_Comps, 'ICA_Components', Data_Type);
+    Filename_Destination = [extractBefore(Filename_Data, Data_Type), 'Deblinked.set'];
+    
+    
+    % skip if file already exists or data doesn't exist yet
+    if ~Refresh && exist(fullfile(Destination, Filename_Destination), 'file')
+        disp(['***********', 'Already did ', Filename_Destination, '***********'])
+        continue
+    elseif ~exist(fullfile(Source_Data, Filename_Data), 'file')
+        disp(['***********', 'No data for ', Filename_Destination, '***********'])
+        continue
+    end
+    
+    
+    %%% Get data ready
+    
+    % load data
+    Data = pop_loadset('filepath', Source_Data, 'filename', Filename_Data); % this is the data where you want to remove the components
+    clc
+    EEG = pop_loadset('filepath', Source_Comps, 'filename', Filename_Comps); % this is the data where components were generated (and save the bad ones)
+    clc % hide filename
+    
+    % remove channels from Data that aren't in EEG
+    Data = pop_select(Data, 'channel', labels2indexes({EEG.chanlocs.labels}, Data.chanlocs));
+    
+    % add CZ
+    Data.data(end+1, :) = zeros(1, size(Data.data, 2));
+    Data.chanlocs(end+1) = CZ;
+    
+    % rereference to average
+    Data = pop_reref(Data, []);
+    
+    %%% interface for selecting components
+    RemoveComps
+    if Break
+        break
+    end
+    
+end
---
+++ functions/eeg/RemoveComps.m
@@ -0,0 +1,101 @@
+% This script gets looped in "E_RemoveComponents" until the user is
+% satisfied with cleanliness of the file.
+
+close all
+clc
+
+if ~Automate
+    % open interface for selecting components
+    pop_selectcomps(EEG, 1:35);
+    
+    disp('press enter to proceed')
+    
+    % wait, only proceed when prompted
+    pause
+    
+end
+
+badcomps = find(EEG.reject.gcompreject); % get indexes of selected components
+clc
+
+if ~Automate % TODO: check if this really needs to happen after identifying "badcomps"
+    
+    % save dataset, now containing new components to remove
+    pop_saveset(EEG, 'filename', Filename_Comps, ...
+        'filepath', Source_Comps, ...
+        'check', 'on', ...
+        'savemode', 'onefile', ...
+        'version', '7.3');
+end
+
+% merge data with component structure
+NewEEG = EEG;
+NewEEG.data = Data.data;
+NewEEG.pnts = Data.pnts;
+NewEEG.xmax = Data.xmax;
+NewEEG.times = Data.times;
+NewEEG.event = Data.event;
+
+% remove components
+NewEEG = pop_subcomp(NewEEG, badcomps);
+
+% low-pass filter
+NewEEG = pop_eegfiltnew(NewEEG, [], Parameters.(Data_Type).lp); % for whatever reason, sometimes ICA removal introduces high frequency noise
+
+% plot outcome
+if CheckOutput
+    Pix = get(0,'screensize');
+    PlotPoints = 100*EEG.srate:300*EEG.srate;
+    if size(NewEEG.data, 2)>PlotPoints(end)
+        eegplot(Data.data(:, PlotPoints), 'spacing', 20, 'srate', NewEEG.srate, ...
+            'winlength', 20, 'position', [0 Pix(4)/2 Pix(3) Pix(4)/2])
+        eegplot(NewEEG.data(:, 100*EEG.srate:300*EEG.srate),'spacing', 20, 'srate', NewEEG.srate, ...
+            'winlength', 20, 'position', [0 0 Pix(3) Pix(4)/2])
+    else % if there's not enough space for plotting 200s, then just look at the whole file
+        eegplot(Data.data, 'spacing', 20, 'srate', NewEEG.srate, ...
+            'winlength', 20, 'position', [0 Pix(4)/2 Pix(3) Pix(4)/2])
+        eegplot(NewEEG.data,'spacing', 20, 'srate', NewEEG.srate, ...
+            'winlength', 20, 'position', [0 0 Pix(3) Pix(4)/2])
+    end
+    
+    pause(5) % wait a little so person can look
+    x = input('Is the file ok? (y/n/s) ', 's');
+else
+    x = 'auto';
+end
+
+% save or loop, depending on response
+switch x
+    case 'y'
+        % save new dataset
+        pop_saveset(NewEEG, 'filename', Filename_Destination, ...
+            'filepath', Destination, ...
+            'check', 'on', ...
+            'savemode', 'onefile', ...
+            'version', '7.3');
+        
+        disp(['***********', 'Finished ', Filename_Destination, '***********'])
+        close all
+        Break = true;
+        
+    case 's'
+        % skip this file, do another one
+        Break = false;
+        
+    case 'auto'
+        % save it and move on to the next one
+        pop_saveset(NewEEG, 'filename', Filename_Destination, ...
+            'filepath', Destination, ...
+            'check', 'on', ...
+            'savemode', 'onefile', ...
+            'version', '7.3');
+        
+        disp(['***********', 'Finished ', Filename_Destination, '***********'])
+        close all
+        Break = false;
+        
+    otherwise
+        % re-do
+        RemoveComponents
+end
+
