88f855f8ce3fd182296e04764502d6bad00408ae
snipeso <snipeso@users.noreply.github.com>
2021-05-04 12:15:04 +0200

    temporary attempt to plot autocorrelations

--- functions/eeg/PlotSpectopo.m
+++ functions/eeg/PlotSpectopo.m
@@ -10,12 +10,12 @@ Bands = [
     ];
 BandLabels = {'delta', 'theta', 'alpha', 'beta'};
 Freqs = 1:.5:30;
-
+% 
 Colormap = parula;
-
+% 
 Data = EEG.data(:, round(Start*EEG.srate):round(Stop*EEG.srate));
-
-
+% 
+% 
 [FFT, ~] = pwelch(Data', [], [], Freqs, EEG.srate);
 FFT = FFT';
 figure('units','normalized','outerposition',[0 0 .4 .7])
@@ -32,3 +32,27 @@ for Indx_B = 1:size(Bands, 1)
     
     
 end
+
+R = corr(Data');
+
+X = [EEG.chanlocs.X];
+Y = [EEG.chanlocs.Y];
+Z = [EEG.chanlocs.Z];
+Distances = sqrt((X-X').^2+(Y-Y').^2+(Z-Z').^2);
+
+% get mean of all neighbors
+R(Distances>60) = nan;
+Mean = nanmean(abs(R))';
+
+% black out all "non neighbors"
+R(Distances>60) = 1;
+
+
+figure('units','normalized','outerposition',[0 0 .75 1])
+imagesc([zeros(size(R, 1), 1), Mean,  zeros(size(R, 1), 1), abs(R)])
+caxis([0 .75])
+yticks(1:size(EEG.data,1))
+yticklabels({EEG.chanlocs.labels})
+colormap(flip(colorcet('fire')))
+colorbar
+axis square
