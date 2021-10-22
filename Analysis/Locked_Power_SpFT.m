384dae0a06386211b100ab34ac1c0254f100f203
Sophia Snipes <snipeso@users.noreply.github.com>
2021-10-22 12:08:28 +0200

    script for getting power of epochs of SpFT

--- Analysis/Locked_Power_SpFT.m
+++ Analysis/Locked_Power_SpFT.m
@@ -2,6 +2,9 @@
 % speaking). This is mostly a quality check on whether speech artifacts are
 % still present, but also in the off chance there's a difference, would be
 % cool.
+clear
+clc
+close all
 
 P = analysisParameters();
 Paths = P.Paths;
@@ -17,7 +20,9 @@ WelchWindow = 2;
 
 % triggers
 Stim_Trig = {'S  3'};
-Retention_Trig = {'S 10'};
+Resp_Trig =  {'S  4'};
+Start_Trig = {'S 10'};
+End_Trig =  {'S 11'};
 
 TotTrials = 20;
 
@@ -36,8 +41,8 @@ Answers_Path = fullfile(Source_Tables, [Task, '_AllAnswers.mat']);
 if  ~Refresh &&  exist(Answers_Path, 'file')
     load(Answers_Path, 'Answers')
 else
-   error('Missing AllAnswers.mat file. To get it, run scripts inside /SpFT_Scoring/')
-
+    error('Missing AllAnswers.mat file. To get it, run scripts inside /SpFT_Scoring/')
+    
 end
 
 Files = deblank(cellstr(ls(Source)));
@@ -55,6 +60,20 @@ for Indx_F = 1:numel(Files)
         continue
     end
     
+        
+    % get trial information
+    Info = split(Filename_Core, '_');
+    Trials = Answers(strcmp(Answers.Participant, Info{1})& ...
+        strcmp(Answers.Session, Info{3}), :);
+    
+    if size(Trials, 1) ~= TotTrials
+        warning(['Something went wrong with trials for ', Filename])
+        continue
+    end
+    
+    % sort trials
+    Trials = sortrows(Trials, 'Trial');
+    
     % load EEG
     EEG = pop_loadset('filename', File, 'filepath', Source);
     
@@ -62,16 +81,17 @@ for Indx_F = 1:numel(Files)
     %%% Set as nan all noise
     [Channels, Points] = size(EEG.data);
     fs = EEG.srate;
-
+    
     
     % set to nan all cut data
     Cuts_Filepath = fullfile(Source_Cuts, [Filename_Core, '_Cuts.mat']);
     EEG = nanNoise(EEG, Cuts_Filepath);
     
-      % remove nonEEG channels
-        EEG = pop_select(EEG, 'nochannel', labels2indexes(P.Channels.Remove, EEG.chanlocs));
-      
-        Chanlocs = EEG.chanlocs;
+    % remove nonEEG channels
+    EEG = pop_select(EEG, 'nochannel', labels2indexes(P.Channels.Remove, EEG.chanlocs));
+    
+    Chanlocs = EEG.chanlocs;
+    
     %%% get power
     
     % epoch trials
@@ -80,55 +100,26 @@ for Indx_F = 1:numel(Files)
     
     nfft = 2^nextpow2(WelchWindow*fs);
     
-    EndPre =  AllTriggerTimes(ismember(AllTriggerTypes, Stim_Trig))-1*fs; % IMPORTANT: there's a 1 s cue period prior to stim, so BL excludes this
-    StartPre = EndPre - nfft;
+    StartReading =  AllTriggerTimes(ismember(AllTriggerTypes, Stim_Trig));
+    EndReading =  AllTriggerTimes(ismember(AllTriggerTypes, Resp_Trig));
     
-    StartEncoding = AllTriggerTimes(ismember(AllTriggerTypes, Stim_Trig))-.1*fs; % this little shift is so that the end encoding isn't partially in the retention, but rather with the cue
-    EndEncoding = StartEncoding + nfft;
+        StartSpeaking =  AllTriggerTimes(ismember(AllTriggerTypes, Start_Trig));
+    EndSpeaking =  AllTriggerTimes(ismember(AllTriggerTypes, End_Trig));
     
-    StartRetentions =  AllTriggerTimes(ismember(AllTriggerTypes, Retention_Trig));
-    MidRetentions = StartRetentions + nfft;
-    EndRetentions = MidRetentions +  nfft;
-    
-    ProbeIndx = find(ismember(AllTriggerTypes, Probe_Trig));
-    StartProbes =  AllTriggerTimes(ProbeIndx);
-    EndProbes = StartProbes + nfft;
-    
-    
-    if TotTrials ~= numel(EndPre) || TotTrials ~= numel(StartRetentions) || TotTrials ~= numel(StartProbes)
-        warning(['Something went wrong with triggers for ', EEG_Filename])
+    if TotTrials ~= numel(StartReading) || TotTrials ~= numel(EndReading) || TotTrials ~= numel(StartSpeaking) || TotTrials ~= numel(EndSpeaking) 
+        warning(['Something went wrong with triggers for ', File])
         continue
     end
     
-    Encoding = PowerTrials(EEG, StartEncoding, StartRetentions, WelchWindow);
-    Retention1 = PowerTrials(EEG, StartRetentions, MidRetentions, WelchWindow);
-    Retention2 = PowerTrials(EEG, MidRetentions, EndRetentions, WelchWindow);
-    [Probe, Freqs] = PowerTrials(EEG, StartProbes, EndProbes, WelchWindow);
+    Reading = PowerTrials(EEG, StartReading, EndReading, WelchWindow);
+    [Speaking, Freqs] = PowerTrials(EEG, StartSpeaking, EndSpeaking, WelchWindow);
     
-    Power = cat(4, Encoding, Retention1, Retention2, Probe);
+    Power = cat(4, Reading, Speaking);
     Power = permute(Power, [3, 4, 1, 2]); % data saved as trial x epoch x ch x freq
-    
 
-    % get trial information
-    Info = split(Filename_Core, '_');
-    Trials = Answers(strcmp(Answers.Participant, Info{1})& ...
-        strcmp(Answers.Session, Info{3}), :);
     
-    if size(Trials, 1) ~= numel(EndPre) || size(Trials, 1) ~= numel(StartRetentions)
-        warning(['Something went wrong with trials for ', EEG_Filename])
-        continue
-    end
-    
-    % get reaction times from triggers
-    RTs = nan(TotTrials, 1);
-    for Indx_T = 1:TotTrials
-        if Trials.missed(Indx_T)
-            continue
-        else 
-            RTs(Indx_T) = abs(diff(AllTriggerTimes([ProbeIndx(Indx_T), ProbeIndx(Indx_T)+1]))/fs);
-        end
-    end
-    Trials.RT = RTs;
+    % get reading times from triggers
+    Trials.RT = (EndReading' - StartReading')/fs;
     
     
     % save
