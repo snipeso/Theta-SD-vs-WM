a0fc1e1175cf95a0916edd1416571ed0c1bf687b
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2021-11-25 11:22:52 +0100

    loop through tasks

--- Preprocessing/F_eeglab2fieldtrip.m
+++ Preprocessing/F_eeglab2fieldtrip.m
@@ -7,8 +7,7 @@ Prep_Parameters
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Task = 'Match2Sample';
-Refresh = false;
+Refresh = true;
 
 Data_Type = 'Power';
 
@@ -23,38 +22,43 @@ EEG_Triggers.End = 'S  2';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Source =  fullfile(Paths.Preprocessed, 'Clean', Data_Type, Task);
-Source_Cuts = fullfile(Paths.Preprocessed, 'Cutting', Cuts_Folder, Task);
 
-Destination = fullfile(Paths.Preprocessed, Destination_Folder, Task);
-
-if ~exist(Destination, 'dir')
-    mkdir(Destination)
-end
-
-Files = deblank(cellstr(ls(Source)));
-Files(~contains(Files, '.set')) = [];
-
-% randomize files list [???]
-nFiles = numel(Files);
-
-for Indx_F = 1:nFiles
+for Indx_T = 1:numel(allTasks)
     
-    Filename = Files{Indx_F};
-    NewFilename = [extractBefore(Filename, '_Clean'), '.mat'];
-    if ~Refresh && exist(fullfile(Destination, NewFilename), 'file')
-        disp(['***********', 'Already did ', Filename, '***********'])
-        continue
+    Task = allTasks{Indx_T};
+    
+    Source =  fullfile(Paths.Preprocessed, 'Clean', Data_Type, Task);
+    Source_Cuts = fullfile(Paths.Preprocessed, 'Cutting', Cuts_Folder, Task);
+    
+    Destination = fullfile(Paths.Preprocessed, Destination_Folder, Task);
+    
+    if ~exist(Destination, 'dir')
+        mkdir(Destination)
     end
-    EEG = pop_loadset('filepath', Source, 'filename', Filename);
     
+    Files = deblank(cellstr(ls(Source)));
+    Files(~contains(Files, '.set')) = [];
     
-    %%% Set as nan all noise
-    % remove nonEEG channels
-    [Channels, Points] = size(EEG.data);
-    fs = EEG.srate;
+    % randomize files list [???]
+    nFiles = numel(Files);
     
-   % remove beginning
+    for Indx_F = 1:nFiles
+        
+        Filename = Files{Indx_F};
+        NewFilename = [extractBefore(Filename, '_Clean'), '.mat'];
+        if ~Refresh && exist(fullfile(Destination, NewFilename), 'file')
+            disp(['***********', 'Already did ', Filename, '***********'])
+            continue
+        end
+        EEG = pop_loadset('filepath', Source, 'filename', Filename);
+        
+        
+        %%% Set as nan all noise
+        % remove nonEEG channels
+        [Channels, Points] = size(EEG.data);
+        fs = EEG.srate;
+        
+        % remove beginning
         if any(strcmpi({EEG.event.type}, EEG_Triggers.Start))
             StartPoint = EEG.event(strcmpi({EEG.event.type}, EEG_Triggers.Start)).latency;
             EEG.data(:, 1:round(StartPoint)) = nan; %this gets removed in rmNoise, which removes anything that's a nan
@@ -69,35 +73,36 @@ for Indx_F = 1:nFiles
         else
             warning('not removing end data...')
         end
-    
-    % set to nan all cut data
-    Cuts_Filepath = fullfile(Source_Cuts, [extractBefore(Filename, '_Clean'), '_Cuts.mat']);
-     EEG = rmNoise(EEG, Cuts_Filepath);
-    
-      EEG = rmNaN(EEG);
-      
-    % remove non-elena channels
-    RmCh = find(ismember({EEG.chanlocs.labels}, [string(EEG_Channels.notSourceLoc), "CZ"]));
-    EEG = pop_select(EEG, 'nochannel', RmCh);
-    
-    % epoch data
-    Starts = 1:Window*fs:Points;
-    Starts(end) = [];
-    Indx = numel(EEG.event)+1;
-    for Indx_S = 1:numel(Starts)
         
-        EEG.event(Indx).latency = Starts(Indx_S);
-        EEG.event(Indx).duration = .5;
-        EEG.event(Indx).channel = 0;
-        EEG.event(Indx).type = 'Epoch_Start';
-        EEG.event(Indx).code = 'edge';
-        Indx = Indx+1;
+        % set to nan all cut data
+        Cuts_Filepath = fullfile(Source_Cuts, [extractBefore(Filename, '_Clean'), '_Cuts.mat']);
+        EEG = rmNoise(EEG, Cuts_Filepath);
+        
+        EEG = rmNaN(EEG);
+        
+        % remove non-elena channels
+        RmCh = find(ismember({EEG.chanlocs.labels}, [string(EEG_Channels.notSourceLoc), "CZ"]));
+        EEG = pop_select(EEG, 'nochannel', RmCh);
+        
+        % epoch data
+        Starts = 1:Window*fs:Points;
+        Starts(end) = [];
+        Indx = numel(EEG.event)+1;
+        for Indx_S = 1:numel(Starts)
+            
+            EEG.event(Indx).latency = Starts(Indx_S);
+            EEG.event(Indx).duration = .5;
+            EEG.event(Indx).channel = 0;
+            EEG.event(Indx).type = 'Epoch_Start';
+            EEG.event(Indx).code = 'edge';
+            Indx = Indx+1;
+        end
+        
+        EEG = pop_epoch(EEG, {'Epoch_Start'}, [0 Window]);
+        
+        Data = eeglab2fieldtrip(EEG, 'raw', 'none');
+        
+        save(fullfile(Destination, NewFilename), 'Data', '-v7.3');
+        
     end
-    
-    EEG = pop_epoch(EEG, {'Epoch_Start'}, [0 Window]);
-    
-    Data = eeglab2fieldtrip(EEG, 'raw', 'none');
-
-    save(fullfile(Destination, NewFilename), 'Data', '-v7.3');
-
 end
