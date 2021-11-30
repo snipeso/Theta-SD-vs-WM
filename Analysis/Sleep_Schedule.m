dbd4207974d48402c848be48590d97c4e0b156be
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2021-11-30 13:12:33 +0100

    get more times

--- Analysis/Sleep_Schedule.m
+++ Analysis/Sleep_Schedule.m
@@ -13,90 +13,109 @@ Format = P.Format;
 Sessions = P.Sessions;
 StatsP = P.StatsP;
 
-CSV = readtable(fullfile(Paths.Preprocessed, 'Final', 'Tables'));
+CSV = readtable(fullfile(Paths.Data, 'Logs', 'Schedule.csv'));
 CSV(~ismember(CSV.ParticipantID, Participants), :) = [];
 
 %%
 %%% BL
 
-Labels = {
-'BL', 'BL_sleep', 'BL_wake_';
-'Pre', 'Main1_sleep', 'Main1_wake';
-'Post', 'Main2_sleep', 'Main2_wake';
-};
+SleepLabels = {
+    'BL', 'BL_sleep', 'BL_wake_';
+    'Pre', 'Main1_sleep', 'Main1_wake';
+    'Post', 'Main2_sleep', 'Main2_wake';
+    };
+
+TaskLabels = {'BL_tasks', 'Session1', 'Session2'};
 
 clc
 
 % Average and range of bed times
-BedTimes = str2time(CSV.(Labels{1, 2}));
+BedTimes = str2time(CSV.(SleepLabels{1, 2}));
 [~, BT_I_Min] = min(timeDiff(6, BedTimes));
 [~, BT_I_Max] = max(timeDiff(6, BedTimes));
 Mean_BT = mean(timeDiff(6, BedTimes));
 Mean_BT = timeDiff(-6, Mean_BT);
 
-disp(['BL Min Bed: ', CSV.(Labels{1, 2}){BT_I_Min}, '; Max Bed: ', CSV.(Labels{1, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
+disp(['BL Min Bed: ', CSV.(SleepLabels{1, 2}){BT_I_Min}, '; Max Bed: ', CSV.(SleepLabels{1, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
 
 
 % average and range of wake times
-WakeTimes = str2time(CSV.(Labels{1, 3}));
+WakeTimes = str2time(CSV.(SleepLabels{1, 3}));
 [~, WT_I_Min] = min(WakeTimes);
 [~, WT_I_Max] = max(WakeTimes);
 Mean_WT = mean(WakeTimes);
 
-disp(['BL Min Wake: ', CSV.(Labels{1, 3}){WT_I_Min}, '; Max Wake: ', CSV.(Labels{1, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
+disp(['BL Min Wake: ', CSV.(SleepLabels{1, 3}){WT_I_Min}, '; Max Wake: ', CSV.(SleepLabels{1, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
 
 
 %%% Pre
 
 % Average and range of bed times
-BedTimes = str2time(CSV.(Labels{2, 2}));
+BedTimes = str2time(CSV.(SleepLabels{2, 2}));
 [~, BT_I_Min] = min(timeDiff(6, BedTimes));
 [~, BT_I_Max] = max(timeDiff(6, BedTimes));
 Mean_BT = mean(timeDiff(6, BedTimes));
 Mean_BT = timeDiff(-6, Mean_BT);
 
-disp(['Pre Min Bed: ', CSV.(Labels{2, 2}){BT_I_Min}, '; Max Bed: ', CSV.(Labels{2, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
+disp(['Pre Min Bed: ', CSV.(SleepLabels{2, 2}){BT_I_Min}, '; Max Bed: ', CSV.(SleepLabels{2, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
 
 
 % average and range of wake times
-WakeTimes = str2time(CSV.(Labels{2, 3}));
+WakeTimes = str2time(CSV.(SleepLabels{2, 3}));
 [~, WT_I_Min] = min(WakeTimes);
 [~, WT_I_Max] = max(WakeTimes);
 Mean_WT = mean(WakeTimes);
 
-disp(['Pre Min Wake: ', CSV.(Labels{2, 3}){WT_I_Min}, '; Max Wake: ', CSV.(Labels{2, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
+disp(['Pre Min Wake: ', CSV.(SleepLabels{2, 3}){WT_I_Min}, '; Max Wake: ', CSV.(SleepLabels{2, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
 
 %%% Post
 
 % Average and range of bed times
-BedTimes = str2time(CSV.(Labels{3, 2}));
+BedTimes = str2time(CSV.(SleepLabels{3, 2}));
 [~, BT_I_Min] = min(BedTimes);
 [~, BT_I_Max] = max(BedTimes);
 Mean_BT = mean(BedTimes);
 
-disp(['Post Min Bed: ', CSV.(Labels{3, 2}){BT_I_Min}, '; Max Bed: ', CSV.(Labels{3, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
+disp(['Post Min Bed: ', CSV.(SleepLabels{3, 2}){BT_I_Min}, '; Max Bed: ', CSV.(SleepLabels{3, 2}){BT_I_Max}, '; Mean Bed: ', time2str(Mean_BT)])
 
 
 % average and range of wake times
-WakeTimes = str2time(CSV.(Labels{3, 3}));
+WakeTimes = str2time(CSV.(SleepLabels{3, 3}));
 [~, WT_I_Min] = min(WakeTimes);
 [~, WT_I_Max] = max(WakeTimes);
 Mean_WT = mean(WakeTimes);
 
-disp(['Post Min Wake: ', CSV.(Labels{3, 3}){WT_I_Min}, '; Max Wake: ', CSV.(Labels{3, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
+disp(['Post Min Wake: ', CSV.(SleepLabels{3, 3}){WT_I_Min}, '; Max Wake: ', CSV.(SleepLabels{3, 3}){WT_I_Max}, '; Mean Wake: ', time2str(Mean_WT)])
 
 
 
 %% average sleep durations
 for Indx_N = 1:3
-    Diff = timeDiff(CSV.(Labels{Indx_N, 2}), CSV.(Labels{Indx_N, 3}));
+    Diff = timeDiff(CSV.(SleepLabels{Indx_N, 2}), CSV.(SleepLabels{Indx_N, 3}));
     Mean = mean(Diff);
-    disp([Labels{Indx_N, 1}, ' mean sleep duration: ',  time2str(Mean) ' [', time2str(min(Diff)) ' ', time2str(max(Diff)) ']'])
+    disp([SleepLabels{Indx_N, 1}, ' mean sleep duration: ',  time2str(Mean) ' [', time2str(min(Diff)) ' ', time2str(max(Diff)) ']'])
 end
 
 
 %%
 % Average BL, S1, SD
+
+for Indx_T = 1:3
+    L = TaskLabels{Indx_T};
+    Times = str2time(CSV.(L));
+    Mean = mean(Times);
+    
+    [~, Min] = min(Times);
+    [~, Max] = max(Times);
+    disp([L, ' Min: ', CSV.(L){Min}, '; Max: ', CSV.(L){Max}, '; Mean: ', time2str(Mean)])
+end
+
+%%
+% BL - WO
+Diff = timeDiff(CSV.(SleepLabels{1, 3}), CSV.(TaskLabels{1}));
+Mean = mean(Diff);
+disp([ 'BL time to task: ',  num2str(Mean*60), '; STD: ', num2str(std(Diff)*60),  ' [', num2str(min(Diff)*60) ' ', num2str(max(Diff)*60) ']'])
+
 
 % average S1 - BL
 
