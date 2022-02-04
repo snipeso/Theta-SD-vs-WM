b5f6038ac039e721d0867180b8855d5c2d3a9fc0
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-02-04 16:51:26 +0100

    cleanup script

--- functions/plots/plotTopoDiff.m
+++ functions/plots/plotTopoDiff.m
@@ -1,8 +1,14 @@
 function Stats = plotTopoDiff(Data1, Data2, Chanlocs, CLims, StatsP, Format)
-% Plot t values of difference between two conditions (Data2 - Data1)
-% Each matrix needs the same number of dimentions; participant x ch
+% plots the t-values (color) and significant channels (white dots) of
+% Data2 vs Data1. 
+% Data are P x Ch matrices.
+% Chanlocs is an EEGLAB channel structure.
+% CLims is the limits for the colormap. If none is provided, then the
+% min/max is used, centered on 0.
+% StatsP is a structure with statistics info (see analysisParameters).
+% Format is a structure with plotting info.
 
-% get t values
+%%% get t values & other info
 Stats = pairedttest(Data1, Data2, StatsP);
 
 ES = Stats.(StatsP.Paired.ES);
@@ -20,11 +26,13 @@ Stats.sigtot = nnz(Sig);
 
 Indexes = 1:numel(Chanlocs);
 
+% get colorlimits
 if isempty(CLims)
     Max = max(abs([quantile(t_values, .01), quantile(t_values, .99)]));
     CLims = [-Max Max];
 end
 
+% plot
 topoplot(Stats.t, Chanlocs, 'maplimits', CLims, 'whitebk', 'on', ...
     'style', 'map',  'plotrad', .72, 'headrad', 'rim', 'gridscale', Format.TopoRes, ...
     'electrodes', 'on', 'emarker2', {Indexes(logical(Sig)), 'o', 'w', Format.Topo.Sig, .05});
@@ -33,16 +41,15 @@ set(gca, 'FontName', Format.FontName)
 xlim([-.55 .55])
 ylim([-.55 .6])
 
+% make all lines same thickness
 A = gca;
-        set(A.Children, 'LineWidth', 1)
-        
+set(A.Children, 'LineWidth', 1)
+
+% change colormap
 Colormap = reduxColormap(Format.Colormap.Divergent, Format.Steps.Divergent);
 colormap(Colormap)
 
-
-
 %%% save in stats the peaks of values
-
 [pks, locs, prom, width] = peakfinder_topo(Stats.hedgesg, [Chanlocs.X], [Chanlocs.Y], [Chanlocs.Z], StatsP.minProminence);
 Labels = str2double({Chanlocs.labels})';
 Peaks = [pks, Labels(locs), prom, width];
