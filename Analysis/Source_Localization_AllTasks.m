79d34b6257badc0850db5096fabd9ad297e6c25a
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-02-22 11:10:35 +0100

    remove figures that are now in chART

--- Analysis/Source_Localization_AllTasks.m
+++ Analysis/Source_Localization_AllTasks.m
@@ -18,8 +18,11 @@ Paths = P.Paths;
 Participants = P.Participants;
 AllTasks = P.AllTasks;
 TaskLabels = P.TaskLabels;
-Pixels = P.Pixels;
 Format = P.Format;
+Format = P.Manuscript;
+Poster = P.Poster;
+Powerpoint = P.Powerpoint;
+Labels = P.Labels;
 
 Folder = fullfile(Paths.Data, 'EEG', 'Source', 'Figure');
 
@@ -34,7 +37,7 @@ if ~exist(Source, 'dir')
 end
 
 
-%%
+
 
 Maps = struct();
 
@@ -59,46 +62,45 @@ end
 
 %% % plot inflated hemispheres for all tasks
 
-Pixels = P.Pixels;
+Format = P.Manuscript;
 
 Grid = [7, 4];
 CLims = [-7 7];
-Pixels.PaddingExterior = 90;
+Format.Figure.Padding = 90;
 plotPatch = true;
-figure('units','centimeters','position',[0 4 Pixels.W Pixels.H])
+figure('units','centimeters','position',[0 4 Format.Figure.Width Format.Figure.Height])
 
 
 for Indx_T = 1:numel(AllTasks)
     
     % plot each face
-    subfigure([], Grid, [Indx_T, 1], [], false, '', Pixels);
+    subfigure([], Grid, [Indx_T, 1], [], false, '', Format);
     plotBalloonBrain(Maps(Indx_T), 'left-outside', CLims, false, Format)
     
     
-   % plot task labels
-     Z = get(gca, 'ZLim');
+    % plot task labels
+    Z = get(gca, 'ZLim');
     text(0, 150, diff(Z)/2 + Z(1), TaskLabels{Indx_T}, ...
-        'FontSize', Pixels.LetterSize, 'FontName', Pixels.FontName, ...
+        'FontSize', Format.Text.IndexSize, 'FontName', Format.Text.FontName, ...
         'FontWeight', 'Bold', 'HorizontalAlignment', 'Center', 'Rotation', 90);
     
     % plot all other balloon sides
-    subfigure([], Grid, [Indx_T, 2], [], false, '', Pixels);
+    subfigure([], Grid, [Indx_T, 2], [], false, '', Format);
     plotBalloonBrain(Maps(Indx_T), 'right-outside', CLims, false, Format)
     
-    subfigure([], Grid, [Indx_T, 3], [], false, '', Pixels);
+    subfigure([], Grid, [Indx_T, 3], [], false, '', Format);
     plotBalloonBrain(Maps(Indx_T), 'left-inside', CLims, plotPatch, Format)
     
-    subfigure([], Grid, [Indx_T, 4], [], false, '', Pixels);
+    subfigure([], Grid, [Indx_T, 4], [], false, '', Format);
     plotBalloonBrain(Maps(Indx_T), 'right-inside', CLims, plotPatch, Format)
 end
 
 % colorbar
-A = subfigure([], Grid, [numel(AllTasks)+1, 1], [1, 4], false, '', Pixels);
-shiftaxis(A, Pixels.PaddingLabels, Pixels.PaddingLabels)
+A = subfigure([], Grid, [numel(AllTasks)+1, 1], [1, 4], false, '', Format);
 
-Pixels.Colorbar = 'north';
-Pixels.BarSize = Pixels.FontSize;
-plotColorbar('Divergent', CLims, Format.Labels.t, Pixels)
+Format.Colorbar.Position = 'north';
+Format.Text.LegendSize = Format.Text.AxisSize;
+plotColorbar('Divergent', CLims, Labels.t, Format)
 
 % save
 saveFig('All_Sources', Paths.Paper, Format)
@@ -114,7 +116,7 @@ load(fullfile(Folder, 'stat_all_tasks_sess2_vs_base.mat'), 'stat')
 
 for Indx_T = 1:numel(AllTasks)
     Mask = stat.(AllTasks{Indx_T}).mask;
-disp([AllTasks{Indx_T}, ' significant voxels: ', num2str(round(100*nansum(Mask)/nnz(~isnan(Mask)))), '%'])
+    disp([AllTasks{Indx_T}, ' significant voxels: ', num2str(round(100*nansum(Mask)/nnz(~isnan(Mask)))), '%'])
     
 end
 
@@ -122,11 +124,58 @@ end
 
 % same for fmTheta and sdTheta
 load(fullfile(Folder, 'stat_M2S_lvl3_vs_lvl1.mat'), 'stat')
-  Mask = stat.mask;
+Mask = stat.mask;
 disp(['fmTheta significant voxels: ', num2str(round(100*nansum(Mask)/nnz(~isnan(Mask)))), '%'])
 
 load(fullfile(Folder, 'stat_M2S_BS_vs_S2_lvl1.mat'), 'stat')
 
-  Mask = stat.mask;
+Mask = stat.mask;
 disp(['sdTheta significant voxels: ', num2str(round(100*nansum(Mask)/nnz(~isnan(Mask)))), '%'])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% GRC poster
+
+%%
+
+Format = P.Poster;
+
+Grid = [1, 4];
+CLims = [-7 7];
+Format.Figure.Padding = 0;
+plotPatch = true;
+figure('units','centimeters','position',[0 4 44 10])
+% figure('units','centimeters','position',[0 4 20 30])
+
+% colorbar
+A = subfigure([], Grid, [1, 1], [1, 4], false, '', Format);
+
+Format.Colorbar.Position = 'south';
+Format.Text.LegendSize = Format.Text.AxisSize;
+plotColorbar('Divergent', CLims, Labels.t, Format)
+% save
+saveFig(['Sources_Colorbar'], Paths.Poster, Format)
+
+%%
+for Indx_T = 1:numel(AllTasks)
+    figure('units','centimeters','position',[0 4 44 10])
+    % plot each face
+    subfigure([], Grid, [1, 1], [], false, '', Format);
+    plotBalloonBrain(Maps(Indx_T), 'left-outside', CLims, false, Format)
+    
+    % plot all other balloon sides
+    subfigure([], Grid, [1, 2], [], false, '', Format);
+    plotBalloonBrain(Maps(Indx_T), 'right-outside', CLims, false, Format)
+    
+    subfigure([], Grid, [1, 3], [], false, '', Format);
+    plotBalloonBrain(Maps(Indx_T), 'left-inside', CLims, plotPatch, Format)
+    
+    subfigure([], Grid, [1, 4], [], false, '', Format);
+    plotBalloonBrain(Maps(Indx_T), 'right-inside', CLims, plotPatch, Format)
+    
+    % save
+    saveFig(['Sources_', AllTasks{Indx_T}], Paths.Poster, Format)
+    
+end
+
 
--- functions/plots/plotColorbar.m
+++
@@ -1,8 +0,0 @@
-function plotColorbar(Colormap, CLims, CLabel,  Format)
-h = colorbar('location', Format.Colorbar);
- ylabel(h, CLabel, 'FontName', Format.FontName, 'FontSize', Format.BarSize)
- h.TickLength = 0;
-caxis(CLims)
-axis off
-colormap(reduxColormap(Format.Colormap.(Colormap), Format.Steps.(Colormap)))
-set(gca, 'FontName', Format.FontName, 'FontSize', Format.BarSize)
