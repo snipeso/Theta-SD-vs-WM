f23e720ad4a779df81c34e4e9787f5f90b594c59
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-03-07 21:56:42 +0100

    Update plotES.m

--- functions/plots/plotES.m
+++ functions/plots/plotES.m
@@ -2,8 +2,6 @@ function Stats = plotES(Data, Orientation, Sort, Colors, xLabels, Legend, PlotPr
 % plot effects sizes as ines with circle in middle
 % Data is a P x S x T matrix
 
-Effects = 0:.5:3; % lines to plot
-
 % get hedge's g stats (because <50 participants)
 Dims = size(Data);
 
@@ -17,18 +15,13 @@ else
     BL = permute(Data(:, 1, :), [1 3 2]);
     SD = permute(Data(:, 2, :), [1 3 2]);
 end
+
 Stats = hedgesG(BL, SD, StatsP);
 
-% Order values based on SD hedge's G
-[~, Order] = sort(mean(Stats.hedgesg, 2));
-
-% plot effect size lines
-% hold on
-% for E = Effects % NB: has to be this way because of axis flipping (i think)
-%     plot( [0, numel(xLabels)+1],[E, E], 'Color', [.9 .9 .9], 'HandleVisibility', 'off')
-% end
-
 if Sort
+    % Order values based on SD hedge's G
+    [~, Order] = sort(mean(Stats.hedgesg, 2));
+    
     plotUFO(Stats.hedgesg(Order, :), Stats.hedgesgCI(Order, :, :), xLabels(Order), Legend, ...
         Colors(Order, :), Orientation, PlotProps)
 else
