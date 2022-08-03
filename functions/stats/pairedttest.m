6e8d5cb1c3b5570ac81517c70501068be752a1bd
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-08-03 19:06:45 +0200

    fixed an error that for some reason never appeared before if missing data

--- functions/stats/pairedttest.m
+++ functions/stats/pairedttest.m
@@ -15,12 +15,12 @@ Dims1 = size(Data1);
 Dims2 = size(Data2);
 
 if isempty(Data2) && numel(Dims1) == 2 % A
-    
+
     pValues = nan(Dims1(2));
     tValues = nan(Dims1(2));
     CI = nan(Dims1(2), Dims1(2), 2);
     df = nan(Dims1(2));
-    
+
     for Indx1 = 1:Dims1(2)-1
         for Indx2 = Indx1+1:Dims1(2)
             [~, p, ci, stats] = ttest(Data1(:, Indx1), Data1(:, Indx2));
@@ -30,7 +30,7 @@ if isempty(Data2) && numel(Dims1) == 2 % A
             CI(Indx1, Indx2, :) = ci;
         end
     end
-    
+
     % get vector of diamond matrix so can replace things properly
     Indexes = 1:Dims1(2)^2;
     Indexes = reshape(Indexes, Dims1(2), []);
@@ -39,10 +39,10 @@ if isempty(Data2) && numel(Dims1) == 2 % A
     Nans = isnan(pValues_long); % there is probably a more elegant way to do this
     pValues_long(Nans) = [];
     Indexes_long(Nans) = [];
-    
+
     % identify still significant values
     [sig, crit_p, ~,  pValues_fdr] = fdr_bh(pValues_long, StatsP.Alpha, StatsP.ttest.dep);
-    
+
     h = nan(Dims1(2));
     h(Indexes_long) = sig;
     Stats.sig = h;
@@ -50,24 +50,24 @@ if isempty(Data2) && numel(Dims1) == 2 % A
     Stats.p = pValues;
     Stats.crit_p = crit_p;
     Stats.df = df;
-    
+
     FDR = nan(Dims1(2));
     FDR(Indexes_long) = pValues_fdr;
     Stats.p_fdr = FDR;
-    
+
     % get effect sizes
     G = hedgesG(Data1, StatsP);
     Stats.hedgesg = G.hedgesg;
-    
+
 elseif isempty(Data2) && numel(Dims1) == 3 % B
     Stats = struct();
-    
+
 elseif numel(Dims1) == 2 && numel(Dims2) == 2 % C
-    
+
     [~, p, CI, stats] = ttest((Data2 - Data1));
     [Sig, crit_p, ~, adj_P] = fdr_bh(p, StatsP.Alpha, StatsP.ttest.dep); % NOTE: dep is good for ERPs, since data can be negatively correlated as well
     t_values = stats.tstat';
-    
+
     Stats.t = t_values(:);
     Stats.p = p(:);
     Stats.p_fdr = adj_P;
@@ -78,37 +78,44 @@ elseif numel(Dims1) == 2 && numel(Dims2) == 2 % C
     Diff = Data2-Data1;
     Stats.mean_diff = nanmean(Diff, 1)';
     Stats.std_diff = nanstd(Diff, 0, 1)';
-    
+
     Stats.mean1 = nanmean(Data1, 1)';
     Stats.std1 = nanstd(Data1, 0, 1)';
     Stats.mean2 = nanmean(Data2, 1)';
     Stats.std2 = nanstd(Data2, 0, 1)';
-    
+
     stats = mes(Data2, Data1, StatsP.Paired.ES, 'isDep', 1);
     ES = stats.(StatsP.Paired.ES)';
     Stats.(StatsP.Paired.ES) = ES;
-    
+
 elseif numel(Dims1) == 2 && numel(Dims2) == 3 % D
-    
+
     % get all p-values
     p = nan(Dims2(2), Dims2(3));
     t_values = p;
     df = p;
     CI = nan(Dims2(2), Dims2(3), 2);
-    
+
     for Indx_S = 1:Dims2(2)
         for Indx_T = 1:Dims2(3)
             D = squeeze(Data2(:, Indx_S, Indx_T));
             BL = squeeze(Data1(:, Indx_T));
-            [~, p(Indx_S, Indx_T), CI(Indx_S, Indx_T, :), stats] = ttest(D(:)-BL(:));
-            df(Indx_S, Indx_T) = stats.df;
-            t_values(Indx_S, Indx_T) = stats.tstat;
+            if nnz(~isnan(D)) < 5 || nnz(~isnan(BL)) < 5
+                p(Indx_S, Indx_T) = nan;
+                CI(Indx_S, Indx_T, :) = nan;
+                df = nan;
+                t_values(Indx_S, Indx_T) = nan;
+            else
+                [~, p(Indx_S, Indx_T), CI(Indx_S, Indx_T, :), stats] = ttest(D(:)-BL(:));
+                df(Indx_S, Indx_T) = stats.df;
+                t_values(Indx_S, Indx_T) = stats.tstat;
+            end
         end
     end
-    
+
     % apply fdr correction
     [Sig, crit_p, ~,  pValues_fdr] = fdr_bh(p, StatsP.Alpha, StatsP.ttest.dep);
-    
+
     % save to stats struct
     Stats.p = p;
     Stats.p_fdr =  pValues_fdr;
