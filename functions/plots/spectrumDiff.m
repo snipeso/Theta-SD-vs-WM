7fb32bd4711beabc58781c5a0cbc5beacb0b14ab
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-10-14 18:06:50 +0200

    little plotting imporvements

--- functions/plots/spectrumDiff.m
+++ functions/plots/spectrumDiff.m
@@ -41,6 +41,7 @@ Max = max(Means(:));
 
 
 %%% Get stats
+if ~isempty(StatsP)
 Data1 = squeeze(Data(:, BL_Indx, :)); % baseline
 Data2 = Data;
 Data2(:, BL_Indx, :) = []; % sessions to compare to the baseline
@@ -48,13 +49,16 @@ Stats = pairedttest(Data1, Data2, StatsP);
 Stats.freqs = Freqs;
 Stats.lines = LineLabels;
 Stats.lines(BL_Indx) = [];
-
 Dims = size(Data1);
 
 Sig = [zeros(1, Dims(2)); Stats.p_fdr < StatsP.Alpha];
+else
+    Dims = size(Data);
+    Sig = zeros(Dims(2), Dims(3));
+end
 
 % plot
-plotGloWorms(squeeze(nanmean(Data, 1)), X, logical(Sig), Colors, PlotProps)
+plotGloWorms(squeeze(mean(Data, 1, 'omitnan')), X, logical(Sig), Colors, PlotProps)
 
 ylim([Min Max])
 ylabel(Labels.zPower)
@@ -65,7 +69,7 @@ h=gca; h.XAxis.TickLength = [0 0];
 h=gca; h.YAxis.TickLength = [0 0];
 
 
-if ~isempty(LineLabels)
+if ~isempty(LineLabels) && ~isempty(StatsP)
     Alpha = num2str(StatsP.Alpha);
     Sig = ['p<', Alpha(2:end)];
     legend([LineLabels, Sig])
